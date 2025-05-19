e362bb850c59350f9f1b8b66b97e79cc2b21855f
Paul Pratt <paul.pratt@nelnet.net>
2025-05-19 11:33:16 -0500

    seeding and persisting

--- App.Interview/App.Interview.Server/Program.cs
+++ App.Interview/App.Interview.Server/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Create a SQLite connection that will be kept open for the lifetime of the application
+var connection = new SqliteConnection("Data Source=:memory:");
+connection.Open();
+
 // Add services to the container.
 builder.Services.AddDbContext<App.Interview.Server.Data.ApplicationDbContext>(options =>
-    options.UseSqlite("Data Source=:memory:"));
+    options.UseSqlite(connection));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,6 +26,17 @@ using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<App.Interview.Server.Data.ApplicationDbContext>();
     context.Database.EnsureCreated();
+    
+    // Verify the database was created
+    var studentsExist = context.Students.Any();
+    var staffExist = context.Staff.Any();
+    var coursesExist = context.Courses.Any();
+    var rostersExist = context.Rosters.Any();
+    
+    if (!studentsExist || !staffExist || !coursesExist || !rostersExist)
+    {
+        throw new Exception("Database initialization failed. Tables were not created properly.");
+    }
 }
 
 // Configure the HTTP request pipeline.
